c699db9b9e60b703f466f2d3007d4b6f5a840468
pavel.malygin <pavel.malygin@nexign-systems.com>
2018-08-16 15:45:46 +0300

    1608_1545

--- ETicket/src/ticket.js
+++ ETicket/src/ticket.js
@@ -1,515 +1,60 @@
-
-web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/kp7Z0DFPGq7d3S2lIKEz"));
-
-// Checking if Web3 has been injected by the browser (Mist/MetaMask)
-//if (typeof web3 !== 'undefined') {
-//   web3 = new Web3(web3.currentProvider); // Use Mist/MetaMask's provider
-//} else {
-//   console.log('No web3? You should consider trying MetaMask!')
-//   //web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-//}
-
-
-
-var payment_value;
-var contractAddr;
-var privKey;
-var addressFrom;
-var contract;
-var contractAbi;
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+var payment_value = "0.01"; //ethereum
+var contractAddr = "0x0c22320FabEC1E1c38Ab700B1a3C4a2ddBDd5D8D";
 
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+window.addEventListener('load', function() {
+   // попробуем работать с MetaMask
+   if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider);
+   } else {
+      alert('No web3? You should consider trying MetaMask!');
+   }
+})
 
-window.onload = function() {
-    payment_value    = document.getElementById("payment_value").value
-    contractAddr     = document.getElementById("contract_address").value
-    privKey          = document.getElementById("private_key").value
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function buyTicket() {
 
-    addressFrom      = web3.eth.accounts.privateKeyToAccount(`${privKey}`).address;
+   var usrName = document.getElementById("usr_name").value;
+   var usrMail = document.getElementById("usr_mail").value;
 
-    setLog(`Sender Address: ${addressFrom}`);
-    contractAbi = [
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_interfaceId",
-            "type": "bytes4"
-          }
-        ],
-        "name": "supportsInterface",
-        "outputs": [
-          {
-            "name": "",
-            "type": "bool"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "name",
-        "outputs": [
-          {
-            "name": "",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "getApproved",
-        "outputs": [
-          {
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "_to",
-            "type": "address"
-          },
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "approve",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "InterfaceId_ERC165",
-        "outputs": [
-          {
-            "name": "",
-            "type": "bytes4"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "_from",
-            "type": "address"
-          },
-          {
-            "name": "_to",
-            "type": "address"
-          },
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "transferFrom",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_owner",
-            "type": "address"
-          },
-          {
-            "name": "_index",
-            "type": "uint256"
-          }
-        ],
-        "name": "tokenOfOwnerByIndex",
-        "outputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "_from",
-            "type": "address"
-          },
-          {
-            "name": "_to",
-            "type": "address"
-          },
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "safeTransferFrom",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "exists",
-        "outputs": [
-          {
-            "name": "",
-            "type": "bool"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_index",
-            "type": "uint256"
-          }
-        ],
-        "name": "tokenByIndex",
-        "outputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "ownerOf",
-        "outputs": [
-          {
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_owner",
-            "type": "address"
-          }
-        ],
-        "name": "balanceOf",
-        "outputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "symbol",
-        "outputs": [
-          {
-            "name": "",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "_to",
-            "type": "address"
-          },
-          {
-            "name": "_approved",
-            "type": "bool"
-          }
-        ],
-        "name": "setApprovalForAll",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "_from",
-            "type": "address"
-          },
-          {
-            "name": "_to",
-            "type": "address"
-          },
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          },
-          {
-            "name": "_data",
-            "type": "bytes"
-          }
-        ],
-        "name": "safeTransferFrom",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "tokenURI",
-        "outputs": [
-          {
-            "name": "",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "_owner",
-            "type": "address"
-          },
-          {
-            "name": "_operator",
-            "type": "address"
-          }
-        ],
-        "name": "isApprovedForAll",
-        "outputs": [
-          {
-            "name": "",
-            "type": "bool"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "name": "_name",
-            "type": "string"
-          },
-          {
-            "name": "_symbol",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-      },
-      {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": true,
-            "name": "_from",
-            "type": "address"
-          },
-          {
-            "indexed": true,
-            "name": "_to",
-            "type": "address"
-          },
-          {
-            "indexed": true,
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "Transfer",
-        "type": "event"
-      },
-      {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": true,
-            "name": "_owner",
-            "type": "address"
-          },
-          {
-            "indexed": true,
-            "name": "_approved",
-            "type": "address"
-          },
-          {
-            "indexed": true,
-            "name": "_tokenId",
-            "type": "uint256"
-          }
-        ],
-        "name": "Approval",
-        "type": "event"
-      },
-      {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": true,
-            "name": "_owner",
-            "type": "address"
-          },
-          {
-            "indexed": true,
-            "name": "_operator",
-            "type": "address"
-          },
-          {
-            "indexed": false,
-            "name": "_approved",
-            "type": "bool"
-          }
-        ],
-        "name": "ApprovalForAll",
-        "type": "event"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "_data",
-            "type": "string"
-          }
-        ],
-        "name": "createNewToken",
-        "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-      }
-    ];
-    contract = new web3.eth.Contract(contractAbi, contractAddr, {from: addressFrom});
-}
+   if(usrName.trim() == "" ||  usrMail.trim() == "") {
+      alert("error: login or e-mail is not defined");
+      return;
+   }
 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   var contractAbi = [{"constant":true,"inputs":[{"name":"_interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"InterfaceId_ERC165","outputs":[{"name":"","type":"bytes4"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"exists","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_name","type":"string"},{"name":"_symbol","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_approved","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_operator","type":"address"},{"indexed":false,"name":"_approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"constant":false,"inputs":[{"name":"_data","type":"string"}],"name":"createNewToken","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}]
 
-function getTocken() {
+   web3.eth.getCoinbase(function(err, addrFrom) {
 
-  encoded = contract.methods.createNewToken("pavel_123456").encodeABI()
+      console.log("addrFrom: " + addrFrom);
+      var contract = new web3.eth.Contract(contractAbi, contractAddr, {from: addrFrom});
+      var newToken = contract.methods.createNewToken(usrName + usrMail).encodeABI();
 
-  web3.eth.getTransactionCount(addressFrom).then( txCount => {
-      var tx = {
-          from: addressFrom,
-          to: contractAddr,
-          data: encoded,
-          value: web3.utils.toHex(web3.utils.toWei(payment_value)), // 0,01 eth == 10 finney
-          nonce: web3.utils.toHex(txCount),
-          gas: 1000000
-      }
+      web3.eth.getTransactionCount(addrFrom).then( txCount => {
 
-      web3.eth.accounts.signTransaction(tx, privKey)
-      .then(signed => {
+         // формируем транзакцию
+         var tx = {
+            from: addrFrom,
+            to: contractAddr,
+            data: newToken,
+            value: web3.utils.toHex(web3.utils.toWei(payment_value)),
+            nonce: web3.utils.toHex(txCount),
+            gas: 1000000
+         }
 
-          web3.eth.sendSignedTransaction(signed.rawTransaction)
-          .on('receipt', res => {
-              console.log(res)
-          })
+         // отправляем транзакцию
+         web3.eth.sendTransaction(tx)
+         .on('transactionHash', function(hash) {
+            console.log("transaction hash: " + hash);
+         })
+         .on('receipt', function(receipt) {
+            console.log(receipt);
+         })
+         .on('confirmation', function(confirmationNumber, receipt) {
+            console.log("confirm num: " + confirmationNumber);
+         })
+         .on('error', console.error);
       })
-  })
-}
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-   function setLog(message) {
-      var current = new Date()
-      var tm = "[" + current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds() + "." + current.getMilliseconds() + "] "
-
-      if (typeof message == 'object') {
-         log.innerHTML += (JSON && JSON.stringify ? tm + JSON.stringify(message) : tm + message) + '<br />'
-      }
-      else {
-         log.innerHTML += tm + message + '<br />'
-      }
+   })
 }
--- README.md
+++ README.md
@@ -3,7 +3,15 @@ cd ETicket
 truffle init
 truffle create contract ETicket
 
-# скачиваем библиотеку openzeppelin-solidity, будем использовать контракты ERC-721
-npm init -y
-npm install -E openzeppelin-solidity
-npm install truffle-hdwallet-provider
+* скачиваем библиотеку openzeppelin-solidity, будем использовать для создания контрактов ERC-721  
+**npm init -y**  
+**npm install -E openzeppelin-solidity**
+
+* установим truffle-hdwallet-provider (пропишем в truffle.js, чтобы можно было коннектиться к ноде через infura)  
+**npm install truffle-hdwallet-provider**
+
+* установим [простой веб сервер](https://www.npmjs.com/package/http-server) для тестов html  
+**npm install http-server -g**
+> запустить и указать путь к директории где лежат html файлы  
+**http-server <путь> -a localhost -p 8000 -c-1**  
+http://localhost:8080/
